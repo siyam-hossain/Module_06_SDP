73d7c77ba23b7f8ed2508e1e7d95987808095cd0
siyam-hossain <siyam.cybersoul@gmail.com>
2025-06-28 20:54:22 +0600

    update2

---
+++ 01_tamplet_litarels_&_spread_operators.js
@@ -0,0 +1,13 @@
+let name = "Siyam Hossain";
+name = "hey";
+
+console.log(name);
+
+
+
+
+const countryName = "Bangladesh";
+// tamplet litarels 
+const country = `My country name is ${countryName}`;
+
+console.log(country);
